0d59df0eaea05b6fac5a06b541ea250921152e38
LemonFruitz <52597213+LemonFruitz@users.noreply.github.com>
2026-06-10 15:04:33 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'starting-shift-v14';
+const CACHE_NAME = 'starting-shift-v15';
 const ASSETS = [
   '/', '/index.html', '/shift.html', '/bd-report.html',
   '/admin.html', '/supervisor.html', '/pjo.html', '/tv.html',
